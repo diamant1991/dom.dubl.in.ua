8e70a8126e90708789049501c5b5cf4d2f082979
diamant1991 <diamant1988@mail.ru>
2015-01-25 18:49:10 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -109,7 +109,20 @@ $(window).scroll(function(){
 
 
     });
+var top_show = 200; 
+  var delay = 1300; 
+  $(document).ready(function() {
+    $(window).scroll(function () { 
+      if ($(this).scrollTop() > top_show) $('#scrollup').fadeIn();
+      else $('#scrollup').fadeOut();
+    });
 
+    $('#scrollup').click(function () {
+      $('body, html').animate({
+        scrollTop: 0
+      }, delay);
+    });
+  });
 
 
 
